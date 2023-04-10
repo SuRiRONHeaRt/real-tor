75b93df3933be5e9e3edcbb68efaf1ee2a8db19f
raveen <raveenranadewa@gmail.com>
2023-04-10 11:37:52 +0530

    Address 3rd time vercel spinner problem

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,11 +1,11 @@
-import spinner from "/Spinner.svg";
+import spinner from "../assets/Spinner.svg";
 
 const Spinner = () => {
   return (
     <div
       className="bg-gray-400 bg-opacity-50 flex 
     items-center justify-center fixed left-0 right-0 
-    bottom-0 top-0 z-50"
+    bottom-0 top-0 z-40"
     >
       <div>
         <img src={spinner} alt="Loading.." className="h-24" />
